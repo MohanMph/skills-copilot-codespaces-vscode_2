4316871b0ef197708028d38a8855eff15e65981d
MohanMph <mohankarthik.r1@mphasis.com>
2024-10-10 03:51:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JavaScript"];
+    var name = "John";
+    var age = 25;
+    return {
+        skills: skills,
+        name: name,
+        age: age
+    };
+}
